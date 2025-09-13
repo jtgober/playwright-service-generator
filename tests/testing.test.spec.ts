28f9f5afab2cff42fc222d774031d98ea9151ce0
Jonathan Gober <Gober.jonathan@gmail.com>
2025-09-13 08:09:43 -0400

    moving request into a constructure for less parameters. updating service and base.ts

--- tests/testing.test.spec.ts
+++ tests/testing.test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from './base';
 
-test('Sample test to verify setup', async ({ request, usersService }) => {
-    const response = await usersService.getUsers(request);
+test('Sample test to verify setup', async ({ usersService }) => {
+    const response = await usersService.getUsers();
     expect(response.status()).toBe(200);
     const users = await response.json();
     console.log(users);
