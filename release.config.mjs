0c587e6636cc70e8b857fb3b29b34aba35d835a6
Jonathan Gober <Gober.jonathan@gmail.com>
2025-09-14 13:32:29 -0400

    reverting back as semver does not work

--- release.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-  branches: ["main"],
-  plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm"
-  ],
-};
